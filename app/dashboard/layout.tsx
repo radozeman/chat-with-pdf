15cbe125b355bff679cf0f824102e7ec94eb92f7
radozeman <r.zeman96@gmail.com>
2024-07-25 00:59:11 +0200

    upload , dashboard, landing page

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import Header from "@/components/Header";
+import { ClerkLoaded } from "@clerk/nextjs";
+
+function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkLoaded>
+      <div className="flex flex-col flex-1 h-screen">
+        <Header />
+        <main className="flex-1 overflow-y-auto">{children}</main>
+      </div>
+    </ClerkLoaded>
+  );
+}
+
+export default DashboardLayout;
